b80fa529302318d2f89ed34929b9a6e60a7c18bd
Kevin Copo <kevincopo7@gmail.com>
2023-09-13 13:33:16 -0500

    Fix-rute

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://snowbot22.gothub.io/Calculadora-de-edad/"
+  base: "https://snowbot22.github.io/Calculadora-de-edad/"
 })
